b215493e6898880f08a4349829b182b0a2abf03e
illyassvvv <ilyasssr119@gmail.com>
2026-04-15 09:22:40 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,9 @@ class DS {
   static const s4 = 4.0;
   static const s6 = 6.0;
   static const s8 = 8.0;
+  static const s10 = 10.0;
   static const s12 = 12.0;
+  static const s14 = 14.0;
   static const s16 = 16.0;
   static const s20 = 20.0;
   static const s24 = 24.0;
